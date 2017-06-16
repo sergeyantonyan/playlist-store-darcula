5f72e1d85fffe4cb64ad4fa0447281c1e80dd9e5
Andrnaik <Andranik.Karaxanyan@tumo.org>
2017-06-16 18:55:54 +0400

    add koa-static

--- app/server.js
+++ app/server.js
@@ -1,15 +1,17 @@
 'use strict';
 var rout = require("./router");
-
 const http  = require('http');
 const Koa = require('koa');
 const render = require('koa-ejs');
+const serve = require('koa-static');
 
 let app = new Koa();
 let path = require('path');
 let dirName = __dirname;
 
 dirName = dirName.replace("app","");
+
+app.use(serve('.'));
 
 render(app, {
     root: path.join(dirName, 'view'),
@@ -18,6 +20,10 @@ render(app, {
     cache: false,
     debug: true
 });
-
 app.use(rout.routes());
+
+app.use(serve(__dirname + '/assets'));
+
 module.exports = app;
+
+
