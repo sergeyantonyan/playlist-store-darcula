1f55869de39c19c313c3fcd47b8666ddb4ad1437
LAB1638\Administrator <mhov.99@gmail.com>
2017-07-04 20:02:25 +0400

    Add insert video function

--- app/youtube.js
+++ app/youtube.js
@@ -26,7 +26,6 @@ var YoutubeAPI = function (clientID, clientSecret, redirectURL, accsesToken, ref
     return new Promise((resolve, reject) => {
       this.youtube.playlists.list({
         part: 'snippet,contentDetails',
-        //id: 'UC3N5y6UWKJaKqoU2b_0MfTQ'
         mine: true
       }, (err, data) => {
         if (err) {
@@ -52,20 +51,49 @@ var YoutubeAPI = function (clientID, clientSecret, redirectURL, accsesToken, ref
           return reject(err);
         }
         if (data) {
-          console.log(data);
+         // console.log(data);
           return resolve(data);
         }
       })
     })
   };
-  this.insertPlaylist = function () {
+
+  this.insertPlaylist = function (title, description) {
     return new Promise((resolve, reject) => {
       this.youtube.playlists.insert({
         part: 'snippet,status',
         resource: {
           snippet: {
-            title: 'pll',
-            description: 'playlist -- playlist'
+            title: title,
+            description: description
+          },
+          status: {
+            privacyStatus: 'private'
+          }
+        }
+      }, (err, data) => {
+        if (err) {
+          console.error('Get playlist insert error: ' + err);
+          return reject(err)
+        }
+
+        // console.log(data.items);
+        return resolve(data)
+      });
+    })
+  };
+
+  this.insertVideo = function (playlistID, videoID) {
+    return new Promise((resolve, reject) => {
+      this.youtube.playlistItems.insert({
+        part: 'snippet,status',
+        resource: {
+          snippet: {
+            playlistId: playlistID,
+            resourceId: {
+              videoId: videoID,
+              kind: 'youtube#video'
+            }
           },
           status: {
             privacyStatus: 'private'
