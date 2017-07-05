37604bb473adba5246a82ffd28d1d89614af1645
LAB1648\Administrator <mhov.99@gmail.com>
2017-07-05 19:43:20 +0400

    add insert function and get all

--- app/database/db.js
+++ app/database/db.js
@@ -1,17 +1,17 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize('darcula', 'root', '', {
-	host: 'localhost',
-	dialect: 'mysql'
+  host: 'localhost',
+  dialect: 'mysql'
 });
 
 sequelize
-	.authenticate()
-	.then(() => {
-		console.log('Database Connection Established');
-	})
-	.catch(err => {
-		console.error('Database Connection Error', err);
-	});	
+  .authenticate()
+  .then(() => {
+    console.log('Database Connection Established');
+  })
+  .catch(err => {
+    console.error('Database Connection Error', err);
+  });
 
 module.exports = sequelize;
